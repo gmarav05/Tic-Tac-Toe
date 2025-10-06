55a1fa16f85c2671610bc28da260e7bb9a9b77ed
Aravind <gmarav005@gmail.com>
2025-10-06 19:13:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function GameBoard() {
+
+    let gameBoard = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
+    
+}
+
+function ControlBoard() {
+
+    let gameBoard = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
+    
+}
