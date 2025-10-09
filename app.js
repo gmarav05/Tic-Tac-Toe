946b93c5a8b58bc290b6cc35f5cc0d55b9d92761
Aravind <gmarav005@gmail.com>
2025-10-09 23:15:10 +0530

    Add Basic functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const  Gameboard =  function (marker) {
+const  Gameboard = (marker) => {
 
     const gameboard = [
     [marker, 0, 0],
@@ -12,4 +12,22 @@ const  Gameboard =  function (marker) {
 };
 
 
-console.log(Gameboard("X"));
+// console.log(Gameboard("X"));
+
+function Player(name, marker) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function() {
+        console.log(this.name);
+    };
+    Gameboard(this.marker);
+}
+
+const player1 = new Player("Arav", "X");
+const player2 = new Player("Alice", "O");
+
+player1.sayName();
+player2.sayName();
