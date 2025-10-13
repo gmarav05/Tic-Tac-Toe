3e3d5aeb3cccf75dedad7efebb1819359019dce1
Aravind <gmarav005@gmail.com>
2025-10-13 23:57:49 +0530

    modify logic

--- app.js
+++ app.js
@@ -1,42 +1,3 @@
-const  Gameboard = (marker) => {
-
-    const gameboard = [
-    [marker, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-    ];
-    
-    console.log(gameboard);
-};
-
-
-// console.log(Gameboard("X"));
-
-function Player(name, marker) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-        console.log(this.name);
-    };
-    Gameboard(this.marker);
-}
-
-// function Player(name, marker) {
-//     const player1_marker = 'X' 
-
-    
-// }
-
-// const player1 = new Player("Arav", "X");
-// const player2 = new Player("Alice", "O");
-
-// player1.sayName();
-// player2.sayName();
-
-
 function Gameboard() {
 
     const rows = 3;
@@ -45,28 +6,110 @@ function Gameboard() {
     const gameboard = [];
 
     for (let i = 0; i < rows; i++) {
-        
         gameboard[i] = [];
 
         for (let j = 0; j < columns; j++) {
             
-            // gameboard[i] = [];                
+            gameboard[i].push(Cell());
         }
-
     }
-
 
     const getBoard = () => gameboard;
 
-    const tokenMarker = (row, column, marker)  => {
+    const addMarker = (row, column, marker)  => {
+        if (gameboard[row][column].getValue() !==0) return
+        gameboard[row][column].addMarker(marker);
+    };
 
+    const printBoard = () => {
+        const boardWithCellValues = gameboard.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
 
-
-
-    }
-
-
+    return { getBoard, addMarker , printBoard };
 
 }
 
 
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => (value = player);
+    const getValue = () => value;
+
+    return { addToken, getValue };
+    
+}
+
+
+function GameController(player1 = "Aravind", player2 = "Nanda") {
+
+    const board = Gameboard();
+
+    const players = [
+        { name: player1, marker : "X" }, 
+        { name: player2, marker : "O" }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`${getActivePlayer().name}'s marker into row ${row + 1}, column${column + 1}`);
+        board.tokenMarker(row, column, getActivePlayer().marker);
+
+    }
+
+    const flatBoard = boardWithCellValues.flat();
+
+    const winningConditions = [
+
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+
+    for (const combination of winningCombinations ) {
+
+        const [a, b, c] = combination;
+        
+        if (flatBoard[a] !== 0 && flatBoard[a] === flatBoard[b] && flatBoard[a] === flatBoard[c]) {
+            console.log(`${getActivePlayer().name }wins!`);
+            board.printBoard();
+            return;
+        }
+
+        switchPlayerTurn();
+        printNewRound();
+
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer,
+    };
+
+
+}
+
+const game = GameController();
